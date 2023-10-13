82d0b2b92eadc2f0c5f7bbd4702bac8c11a411e8
RaptoRabbit <al3bogd@ya.ru>
2023-10-13 10:02:10 +0300

    Простое число

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log(`0 = False\n1 = True\n-1 = Number 1 `);
+const issimp = (n) =>{
+    if (n=== 1) {
+        return -1
+    }
+    let flage = 1;
+    for (let i = 2; i <= n-1; i++) {
+        if(n % i === 0){
+            flag = 0;
+            break;
+        }        
+    }
+    return flage
+}
+console.log(issimp(6));
